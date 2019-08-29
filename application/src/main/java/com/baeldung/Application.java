0dffada9ac1d6538ee72a657637e86346773d741
Afshin <mnafshin@gmail.com>
2019-08-29 20:02:29 +0200

    Current User name component to greet to is added

--- application/src/main/java/com/baeldung/Application.java
+++ application/src/main/java/com/baeldung/Application.java
@@ -1,13 +1,10 @@
 package com.baeldung;
 
-import com.baeldung.service.impl.GreetingPortStaticAdapter;
-import com.baeldung.service.impl.NamingPortStaticAdapter;
-import com.baeldung.service.impl.OutputStreamingPortConsoleAdapter;
-import com.baeldung.service.impl.TranslatingPortMapAdapter;
+import com.baeldung.service.impl.*;
 
 public class Application {
     public static void main(String[] args) {
-        new Domain(new OutputStreamingPortConsoleAdapter(), new NamingPortStaticAdapter(),
+        new Domain(new OutputStreamingPortConsoleAdapter(), new NamingPortCurrentLoginUserAdapter(),
                 new GreetingPortStaticAdapter(), new TranslatingPortMapAdapter()).greeting();
     }
 }
