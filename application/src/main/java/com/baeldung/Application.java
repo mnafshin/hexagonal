4c2d4cbe6f3f8a50b67aeabaf2dba5b9248429c1
Afshin <mnafshin@gmail.com>
2019-08-29 14:45:48 +0200

    Domain module is extracted

--- application/src/main/java/com/baeldung/Application.java
+++ application/src/main/java/com/baeldung/Application.java
@@ -2,6 +2,6 @@ package com.baeldung;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        new Domain().greeting();
     }
 }
