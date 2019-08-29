d4d2e9c22caebecaa8ee2ae5c3e1959ae66d353d
Afshin <mnafshin@gmail.com>
2019-08-29 19:52:28 +0200

    Greeting port and Translating port are added

--- application/src/main/java/com/baeldung/Application.java
+++ application/src/main/java/com/baeldung/Application.java
@@ -1,10 +1,13 @@
 package com.baeldung;
 
+import com.baeldung.service.impl.GreetingPortStaticAdapter;
 import com.baeldung.service.impl.NamingPortStaticAdapter;
 import com.baeldung.service.impl.OutputStreamingPortConsoleAdapter;
+import com.baeldung.service.impl.TranslatingPortMapAdapter;
 
 public class Application {
     public static void main(String[] args) {
-        new Domain(new OutputStreamingPortConsoleAdapter(), new NamingPortStaticAdapter()).greeting();
+        new Domain(new OutputStreamingPortConsoleAdapter(), new NamingPortStaticAdapter(),
+                new GreetingPortStaticAdapter(), new TranslatingPortMapAdapter()).greeting();
     }
 }
